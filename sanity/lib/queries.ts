26911e599ea7fb34128ff5eae86a6b9cbacf48a4
M3fjuLeo <mateuszropek@onet.pl>
2024-11-22 13:08:12 +0100

    Implement Startup Details

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -18,10 +18,38 @@ export const STARTUPS_QUERY = defineQuery(`*[
     _id, 
     name, 
     image, 
-    bio
+    bio,
+    username
   },
   views,
   description,
   category,
   image
 }`);
+
+export const STARTUP_BY_ID_QUERY = defineQuery(`*[
+  _type == "startup" && _id == $id][0]{
+  _id,
+  title,
+  slug,
+  _createdAt,
+  author -> {
+    _id, 
+    name, 
+    image, 
+    bio,
+    username
+  },
+  views,
+  description,
+  category,
+  image,
+  pitch
+}`);
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+  *[_type == "startup" && _id == $id][0]{
+    _id, 
+    views
+  }
+`);
